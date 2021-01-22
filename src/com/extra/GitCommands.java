65b43e39d11d1a7c3138bf44c09b834cfe8ee4a1
shimul0073 <shossain0073@gmail.com>
2021-01-21 20:08:30 -0600

    update from machine 2

--- src/com/extra/GitCommands.java
+++ src/com/extra/GitCommands.java
@@ -2,9 +2,22 @@ package com.extra;
 
 public class GitCommands {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	// Before start work == To pull the code from another machine 1
+	/*
+	 * go to eclipse-workspace\InterviewPreparation 
+	 * git pull origin master
+	 * Code will be updated from another machine 
+	 */
+		
+	//  After work done == To push the code from machine 1 to 2
+	
+	/*
+	 * git add * 
+	 * git commit -m "any message" 
+	 * git push origin master 
+	 * done with puhsing to the github
+	 */
 	}
 
-}
+
